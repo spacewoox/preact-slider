3822441c769a309d013420a6395ffd101123454c
Eric Cao <spacewoox@gmail.com>
2018-01-05 11:10:11 +0100

    fix column slider not displayed properly

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import ResponsiveSlider from './src/ResponsiveSlider'
 import Slider from './src/Slider'
 
+export {
+  ResponsiveSlider,
+  Slider
+}
+
 export default {
   ResponsiveSlider,
   Slider
--- src/Slider/index.js
+++ src/Slider/index.js
@@ -6,22 +6,40 @@ import WithSliding from '../WithSliding'
 import { cycleThrough } from '../lib/Pagination'
 import { toArrayOfXElement } from '../lib/Utils'
 import { generateSortedMediaQueriesFromList } from '../lib/Json2MqParsing'
-import './style.css'
+import styles from './style.css'
 
-const GenerateSliderList = ({itemPerPage, data, children }) => {
+const GenerateSliderList = ({itemPerPage, data, display, children }) => {
+
+  const generateItem = content => {
+    let style;
+    if (display === 'row') {
+      style = {
+        width: (100 / itemPerPage) + '%',
+        display: 'inline-block'
+      }
+    }
+    else if (display === 'column') {
+      style = {
+        width: '100%',
+        display: 'block'
+      }
+    }
+    return <div className={'slider-item ' + styles.sliderItem} style={style}>{content}</div>
+  }
+
   const list =
     toArrayOfXElement(
-      data.map(x =>
-        <div className='slider-item' style={{width: (100 / itemPerPage) + '%'}}>{x}</div>
-      ), itemPerPage)
-    .map(x => <div className='slider-page'>{x}</div>)
+      data.map(x => generateItem(x)),
+      itemPerPage
+    )
+    .map(x => <div className={'slider-page ' + styles.sliderPage}>{x}</div>)
 
   return children[0](list)
 }
 
-const Slider = ({data, itemPerPage=data.length, children}) => {
+const Slider = ({data, display='row', itemPerPage=data.length, children}) => {
   return (
-    <GenerateSliderList itemPerPage={itemPerPage} data={data}>
+    <GenerateSliderList itemPerPage={itemPerPage} data={data} display={display}>
     {list => (
 
       <WithPaginate max={list.length-1} min={0}>
@@ -29,7 +47,7 @@ const Slider = ({data, itemPerPage=data.length, children}) => {
 
         <WithSliding list={list} offset={offset}>
         {() => (
-          <div className='slider-container'>
+          <div className={'slider-container ' + styles.sliderContainer}>
             {children[0](list, next, prev, offset)}
           </div>
         )}
